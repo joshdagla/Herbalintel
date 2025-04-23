de4fc9fd9e4fdfbdb3d38cfc1b765f17f92c2c79
JoshDagla <115857011+joshdagla@users.noreply.github.com>
2025-04-23 20:56:29 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
     const handleFormSubmit = async (formData) => {
         try {
-            const response = await fetch('http://127.0.0.1:8000/predict/', {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/predict/`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
